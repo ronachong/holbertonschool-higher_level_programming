7fa13b6308c1308b620c571e650acbf24584d67c
Rona Chong <ronachong@gmail.com>
2016-04-03 20:05:30 +0000

    Add line regarding open() to create file to write response.

---
+++ discover_more_python/write_them_to_disc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import urllib2
+
+# create empty file '20' in tmp
+file = open('/tmp/20', 'a')
+
+# create HTTP request
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
+request_headers = {
+      'User-Agent': 'Holberton_School',
+      'Authorization': 'token a2eaac77d1b665a2f1fea8ce17cd48178cd21a69'
+    }
+req = urllib2.Request(url, None, request_headers)
+response = urllib2.urlopen(req)
+
+# write response to /tmp/20
+file.write(response)
