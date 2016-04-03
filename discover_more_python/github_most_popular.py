273a95e8cb528e5ad61b6c7f77ce525611b08073
Rona Chong <ronachong@gmail.com>
2016-04-02 18:35:33 -0700

    Use urllib2 with None for body parameter to make HTTP req.

---
+++ discover_more_python/github_most_popular.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import urllib2
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
+request_headers = {
+      'User-Agent': 'Holberton_School',
+      'Authorization': 'token a2eaac77d1b665a2f1fea8ce17cd48178cd21a69'
+    }
+req = urllib2.Request(url, None, request_headers)
+response = urllib2.urlopen(req)
+
+print response.read()
