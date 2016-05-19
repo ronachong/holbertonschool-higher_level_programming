bd28abb82e91b7e42e7962a46dfbefe0a55e3cc2
Rona Chong <ronachong@gmail.com>
2016-05-19 02:12:10 -0700

    Try to create json file to test new methods.

--- oop_can_manage_your_family/3-main.py
+++ oop_can_manage_your_family/3-main.py
@@ -1,6 +1,16 @@
 from family import Person, Baby, Teenager, Adult, Senior
 from family import load_from_file, save_to_file
 
+# new adult
+a = Adult(0, "Abigail", [10, 24, 1990], "Female", "Blue")
+c = Adult(1, "Carl", [7, 15, 1992], "Male", "Brown")
+d = Senior(2, "Darlene", [3, 4, 1968], "Female", "Brown")
+
+json_hash = {'a': Person.json(a), 'c': Person.json(c), 'd': Person.json(d)}
+print json_hash
+
+save_to_file(json_hash, "my_family.json")
+
 my_family = load_from_file("my_family.json")
 print "I have %d members in my family" % len(my_family)
 
