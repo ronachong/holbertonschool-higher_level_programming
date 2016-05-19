cc368ddf30b0771b199be0d4234f8f8fe94f65db
Rona Chong <ronachong@gmail.com>
2016-05-19 02:36:58 -0700

    Refactor code to create 'json_list' instead of 'json_hash'.
    Although a hash/dict seems to follow the standard appearance
    of json data examples online, it does not follow the intentions
    of the task parameters. Compiling 3-main.py with 'json_hash'
    results in an error from trying to append to a hash. This in-
    dicates that the data structure truly needs to be a list.
    
    Compiling 3-main.py with 'json_list' succeeds and using a list
    with save_to_file/json.dump does not appear to pose an issue.

--- oop_can_manage_your_family/3-main.py
+++ oop_can_manage_your_family/3-main.py
@@ -6,17 +6,20 @@ a = Adult(0, "Abigail", [10, 24, 1990], "Female", "Blue")
 c = Adult(1, "Carl", [7, 15, 1992], "Male", "Brown")
 d = Senior(2, "Darlene", [3, 4, 1968], "Female", "Brown")
 
-json_hash = {'a': Person.json(a), 'c': Person.json(c), 'd': Person.json(d)}
-print json_hash
+json_list = []
+json_list.append(Person.json(a))
+json_list.append(Person.json(c))
+json_list.append(Person.json(d))
+print json_list
 
-save_to_file(json_hash, "my_family.json")
+save_to_file(json_list, "my_family.json")
 
-my_family = load_from_file("my_family.json")
-print "I have %d members in my family" % len(my_family)
+#my_family = load_from_file("my_family.json")
+#print "I have %d members in my family" % len(my_family)
 
 # new baby!
-b = Baby(3, "Tony", [7, 4, 2015], "Male", "Green")
-b.last_name = "Foto"
-my_family.append(b)
+#b = Baby(3, "Tony", [7, 4, 2015], "Male", "Green")
+#b.last_name = "Foto"
+#my_family.append(b)
 
-save_to_file(my_family)
+#save_to_file(my_family)
