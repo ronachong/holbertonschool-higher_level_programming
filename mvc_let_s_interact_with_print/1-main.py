9f9b7ab12e40af2119c00b4be3efc9171a50ca48
Rona Chong <ronachong@gmail.com>
2016-05-22 20:30:37 -0700

    Add provided main functions.

---
+++ mvc_let_s_interact_with_print/1-main.py
@@ -0,0 +1,9 @@
+from task_model import TaskModel
+from task_view import TaskView
+
+
+root = tk.Tk()
+root.withdraw()
+tv = TaskView(root)
+tv.update_title("Finish this funny project")
+root.mainloop()
