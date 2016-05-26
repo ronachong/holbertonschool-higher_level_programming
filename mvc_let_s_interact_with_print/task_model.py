bf40dfed84c9644a38af8617949ee08fddeb5549
Rona Chong <ronachong@gmail.com>
2016-05-25 18:35:26 -0700

    Fix 'null' assignments and identation.
    Null does not exist in Python, but None signifies noneness.
    Replace null with None.
    
    Fix identation in 0-main.py (improper formatting by emacs).

--- mvc_let_s_interact_with_print/task_model.py
+++ mvc_let_s_interact_with_print/task_model.py
@@ -5,26 +5,25 @@ class TaskModel:
             raise Exception("title is not a string")
 
         self.__title = title
-        self.callback_title = null # callback_title is assigned its value by set_callback_title.
+        self.callback_title = None # callback_title is assigned its value by
+        # set_callback_title.
 
-    # magic methods
     def __str__(self):
         print self.__title
 
-    # getter method
     def get_title(self):
         return self.__title
 
-    # setter method
     def set_callback_title(self, value):
     # set_callback_title assigns a value to self.callback_title
         self.__callback_title = value
 
-    # other methods
     def toggle(self):
-    # toggle_self reverses the value of title and calls the callback assigned to self.callback_title
-        self.__title = self.__title[::-1] # uses string slicing to step backwards thru string
+    # toggle_self reverses the value of title and calls the callback assigned to
+    # self.callback_title
+        self.__title = self.__title[::-1] # uses string slicing to step backwards
+        # thru string
 
         # if self.__callback_title is set:
-        if self.__callback_title != null:
+        if self.__callback_title != None:
             self.__callback_title(self.__title)
