1c07f4cbc25db7d867f354aa8fc8bbdd3271e5f2
Rona Chong <ronachong@gmail.com>
2016-05-25 18:23:51 -0700

    Refactor TaskModel.
    callback_title is specified as a private attribute and functions
    as a callback function which is set by set_callback_title.
    Revise comments and code to reflect this.

--- mvc_let_s_interact_with_print/task_model.py
+++ mvc_let_s_interact_with_print/task_model.py
@@ -3,31 +3,28 @@ class TaskModel:
     def __init__(self, title):
         if title = "" or type(title) != str:
             raise Exception("title is not a string")
-        
-        self.__title = title
 
-    def __callback_title(self): # should this be an attribute like self.__callback_title = def, or something similar to that,
-        # or should it be like this, a definition of a private method?
-        # later on set_callback_title seems to exist to change what self.__callback_title is, but how does it accomplish that?
-        [..]
-        
+        self.__title = title
+        self.callback_title = null # callback_title is assigned its value by set_callback_title.
+
     # magic methods
     def __str__(self):
         print self.__title
-        
+
     # getter method
     def get_title(self):
         return self.__title
 
     # setter method
     def set_callback_title(self, value):
-        # what does this function do? set the callback that deals with title?
-        # not sure what to put here; self.__callback_title = value, or some more circuitous operation
+    # set_callback_title assigns a value to self.callback_title
+        self.__callback_title = value
 
     # other methods
     def toggle_self:
+    # toggle_self reverses the value of title and calls the callback assigned to self.callback_title
         self.__title = self.__title[::-1] # uses string slicing to step backwards thru string
 
         # if self.__callback_title is set:
-        if self.__callback_title != ??:
+        if self.__callback_title != null:
             self.__callback_title(self.__title)
