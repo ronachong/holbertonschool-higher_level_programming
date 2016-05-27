03424a42b4600b89e1b53354d3cb7f2f29a69941
Rona Chong <ronachong@gmail.com>
2016-05-27 14:29:32 -0700

    Define Taskview instance attributes with Tkinter methods.
    Use StringVar(), Label() and Button() methods.
    Refer to these functions with self, since they should be
    instance methods via inheritance. (But this is an assump-
    tion).

--- mvc_let_s_interact_with_print/task_view.py
+++ mvc_let_s_interact_with_print/task_view.py
@@ -2,24 +2,36 @@ import Tkinter as tk
 
 # description of View
 class TaskView(tk.Toplevel): # inherit from Toplevel widget/Tinker class
-    __init__(self, master):
-        if master is not tk.Tk(): # not sure if this code needs more iteration
+    def __init__(self, master):
+        if master is not self.Tk(): # if master is not a tk root widget;
+                                    # not sure if code needs more iteration.
+                                    # do I use tk, or self, since self inherits from tk?
             raise Exception("master is not a tk.Tk()")
 
         # else
         tk.Toplevel.__init(self, master) # init attributes according to Toplevel..?
-        self.protocol('WM_DELETE_WINDOW', self.master.destroy) # idk what this does.
-        # probably call protocol defined in Toplevel to destroy the master - but don't
-        # we need the old master...?
+        self.protocol('WM_DELETE_WINDOW', self.master.destroy)  # idk what this does.
+                                                                # probably call protocol defined in Toplevel to destroy
+                                                                # the master - but don't we need the old master...?
 
-        # private attributes
-        self._title_var = ?? # type = tk.StringVar(), i.e. string widget
-        self._title_label = ?? # type = tk.Label(), i.e. label widget
+        # set private attributes
+        self._title_var = self.StringVar() # type = tk.StringVar(), i.e. string widget
+        self._title_label = self.Label(master, text=self._title_var) # type = tk.Label(), i.e. label widget
 
-        # public attributes
-        self.toggle_button = ?? # tk.Button, i.e. a button widget, text = Reverse? and left
+        # set public attributes
+        self.toggle_button = self.Button(
+            frame, text="reverse"
+        ) # tk.Button, i.e. a button widget, text = Reverse?
+
+        # pack widgets
+        self._title_label.pack(side=RIGHT)
+        self.toggle_button.pack(side=LEFT)
 
     # instance methods
     def update_title(self, title):
-        # exception handling
+        # exception raising
+        if type(title) != str:
+            raise Exception("title is not a string")
         # probably update self.title_var; maybe more
+        self._title_var.set(title)
+        # if update_title is run, will the title_label text update as well? or does it need to be manually updated?
