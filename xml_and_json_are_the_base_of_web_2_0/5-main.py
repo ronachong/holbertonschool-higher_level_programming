15278b52c67e862b90ad0cbd83b06a077be617a8
Rona Chong <ronachong@gmail.com>
2016-06-02 00:23:50 -0700

    Add code to create DOM document with cars from file in 5-main.py.
    Current code throws an error and needs refactoring.

--- xml_and_json_are_the_base_of_web_2_0/5-main.py
+++ xml_and_json_are_the_base_of_web_2_0/5-main.py
@@ -1,4 +1,5 @@
 import json
+from xml.dom import minidom
 from car import Car
 
 # read file with filename 5-main.json; store in <variable>
@@ -11,6 +12,11 @@ brands = []
 # create int for cumulative no. of doors for all cars
 cnb_doors = 0
 
+# create root element for DOM document which will contain all cars
+doc = minidom.Document()
+cars = doc.createElement("cars")
+doc.appendChild(cars)
+
 # for every car hash in <variable>, call car constructor with hash as dictionary
 for i in file_contents:
     car = Car(i)
@@ -18,6 +24,7 @@ for i in file_contents:
     # get brand of car, number of doors
     brand = car.get_brand()
     nb_doors = car.get_nb_doors()
+    car_xml = car.to_xml_node(cars)
 
     # check if brand has already been accounted for in brands
     # (if not, add)
@@ -27,6 +34,9 @@ for i in file_contents:
     # add number of doors from car to cnb_doors
     cnb_doors += nb_doors
 
+    # add car element to cars xml
+    cars.appendChild(car_xml)
+
 # print number of different brands, print cumulative number of doors
 print len(brands)
 print cnb_doors
@@ -34,3 +44,6 @@ print cnb_doors
 # print description of last car
 last_car = Car(file_contents[-1])
 print last_car
+
+# print cars xml document
+print cars
