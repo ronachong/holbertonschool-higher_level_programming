e2a394dce7c4c0c925a37b93f8c16cc29f185814
Rona Chong <ronachong@gmail.com>
2016-06-02 00:06:53 -0700

    Write 5-main.py to display info about a json file per Task 5.
    Use open, read, and json.loads to parse contents of 5-main.json
    into local data structure, file_contents.
    Create brands and cnb_doors as holder vars for each unique car
    brand and cumulative number of doors, respectively.
    
    Parse file_contents further using Car class and getter methods, to
    add each unique brand to brands and each nb of doors to cnb_doors.
    
    Use magic __str__ fxn of Car to print description of last car in
    file_contents.
    
    Still to do: create a DOM document with all the cars and print it.

--- xml_and_json_are_the_base_of_web_2_0/5-main.py
+++ xml_and_json_are_the_base_of_web_2_0/5-main.py
@@ -1,3 +1,36 @@
+import json
+from car import Car
+
 # read file with filename 5-main.json; store in <variable>
-# call car constructor with <variable> as dictionary
-# use get method and len method 
+file = open("5-main.json")
+file_contents = json.loads(file.read())
+
+# create array for brands
+brands = []
+
+# create int for cumulative no. of doors for all cars
+cnb_doors = 0
+
+# for every car hash in <variable>, call car constructor with hash as dictionary
+for i in file_contents:
+    car = Car(i)
+
+    # get brand of car, number of doors
+    brand = car.get_brand()
+    nb_doors = car.get_nb_doors()
+
+    # check if brand has already been accounted for in brands
+    # (if not, add)
+    if brand not in brands:
+        brands.append(brand)
+
+    # add number of doors from car to cnb_doors
+    cnb_doors += nb_doors
+
+# print number of different brands, print cumulative number of doors
+print len(brands)
+print cnb_doors
+
+# print description of last car
+last_car = Car(file_contents[-1])
+print last_car
