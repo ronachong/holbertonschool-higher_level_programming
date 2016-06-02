f57c9d8ae30a36e11a1e803c48b93e907b0751dc
Rona Chong <ronachong@gmail.com>
2016-06-01 19:41:33 -0700

    Complete Car constructor.
    Use kwargs instead of args for hash variable, since example call
    to constructor in 2-main.py passes keyworded arguments.

--- xml_and_json_are_the_base_of_web_2_0/car.py
+++ xml_and_json_are_the_base_of_web_2_0/car.py
@@ -1,18 +1,22 @@
 class Car:
     def __init__(self, *args, **kwargs):
-        print "args is equal to " + str(args)
-        print "kwargs is equal to " + str(kwargs)
-        '''if len(args) > 0 and isinstance(args[0], dict):
-        # assuming that hash is passed as first arg
-            hash = args[0]
+        if len(kwargs) > 0:
+            # assume that kwargs is the hash of properties
+            hash = kwargs
             # get values from hash
             name = hash.get('name')
             brand = hash.get('brand')
-            nb_door = hash.get('nb_door')
+            nb_doors = hash.get('nb_doors')
 
+        # raise exceptions given improper argument values
         if name == "" or type(name) != str:
             raise Exception("name is not a string")
         if brand == "" or type(brand) != str:
             raise Exception("brand is not a string")
         if type(nb_doors) != int or nb_doors <= 0:
-            raise Exception("nb_doors is not > 0")'''
+            raise Exception("nb_doors is not > 0")
+
+        # else set private attributes according to parameters
+        self.__name = name
+        self.__brand = brand
+        self.__nb_doors = nb_doors
