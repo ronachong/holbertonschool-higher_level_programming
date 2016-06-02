b3325e04806d54bf9c44b359bab310a036549861
Rona Chong <ronachong@gmail.com>
2016-06-01 19:32:32 -0700

    Identify args and kwargs value in context of 2-main.py.

--- xml_and_json_are_the_base_of_web_2_0/2-main.py
+++ xml_and_json_are_the_base_of_web_2_0/2-main.py
@@ -4,8 +4,3 @@ c = Car(name="Rogue", brand="Nissan", nb_doors=5)
 print c
 print c.get_brand()
 print c.to_hash()
-
-guillaume@production-503e7013:$ python 2-main.py
-Rogue Nissan (5)
-Rogue
-{'nb_doors': 5, 'brand': 'Nissan', 'name': 'Rogue'}
--- xml_and_json_are_the_base_of_web_2_0/car.py
+++ xml_and_json_are_the_base_of_web_2_0/car.py
@@ -1,10 +1,18 @@
 class Car:
     def __init__(self, *args, **kwargs):
+        print "args is equal to " + str(args)
+        print "kwargs is equal to " + str(kwargs)
+        '''if len(args) > 0 and isinstance(args[0], dict):
+        # assuming that hash is passed as first arg
+            hash = args[0]
+            # get values from hash
+            name = hash.get('name')
+            brand = hash.get('brand')
+            nb_door = hash.get('nb_door')
+
         if name == "" or type(name) != str:
             raise Exception("name is not a string")
         if brand == "" or type(brand) != str:
             raise Exception("brand is not a string")
         if type(nb_doors) != int or nb_doors <= 0:
-            raise Exception("nb_doors is not > 0")
-
-        
+            raise Exception("nb_doors is not > 0")'''
