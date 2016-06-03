afb5f047b23f785f72a63200b8c864c5ac993ca8
Rona Chong <ronachong@gmail.com>
2016-06-02 22:29:38 -0700

    Write code to modify 3-main.db per project specs.
    update the age of Jon Snow to 27
    update the age of Walter Junior White to 18
    delete the person Walter White from Person and EyesColor
    display all Person with all attributes ordered by first_name (A->Z)

---
+++ basic_sql_playground/3-main.sql
@@ -0,0 +1,6 @@
+UPDATE Person set age=27 WHERE first_name = 'Jon' AND last_name = 'Snow';
+UPDATE Person set age=18 WHERE first_name = 'Walter Junior' AND last_name = 'White';
+DELETE FROM Person WHERE first_name = 'Walter' AND last_name = 'White';
+DELETE FROM EyesColor WHERE first_name = 'Walter' AND last_name = 'White';
+SELECT * FROM Person
+       ORDER BY first_name ASC;
