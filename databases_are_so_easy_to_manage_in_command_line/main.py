1bfb59b04995bfece9a17db400e6e479ff6bfa69
Rona Chong <ronachong@gmail.com>
2016-06-11 05:00:37 +0000

    Refactor code to use hashes for commands and models.

--- databases_are_so_easy_to_manage_in_command_line/main.py
+++ databases_are_so_easy_to_manage_in_command_line/main.py
@@ -1,68 +1,76 @@
 from sys import *
 from models import *
-# from peewee import *
 
-    
+
 # HELPER METHODS
-def print_table():
-    if argv[2] == "basemodel": print_records(BaseModel)
-    elif argv[2] == "school": print_records(School)
-    elif argv[2] == "batch": print_records(Batch)
-    elif argv[2] == "user": print_records(User)
-    elif argv[2] == "student": print_records(Student)
-        
-def print_records(table):
-    for record in table.select(): print record
-# not sure if record will map back onto Python instance of class?
-# or do I manually print each of the values from the record?        
+def run_create():
+    # create all tables related to models
+    my_models_db.connect()
+    my_models_db.create_tables(models.values())
+    # not sure of the merits of try/except clauses here
 
-def delete_record(table):
+def run_print():
+    # print data for each record in specified table
+    for record in table.select(): print record
+
+def run_insert():
+    if table_key == "school": record = School.create(name=argv[3])
+    elif table_key == "batch": record = Batch.create(school=argv[3], name=argv[4])
+    elif table_key == "student" and len(argv) == 7: record = Student.create(
+            batch=argv[3], \
+            age=argv[4], \
+            last_name=argv[5], \
+            first_name=argv[6])
+    elif table_key == "student" and len(argv) == 6: record = Student.create(
+            batch=argv[3], \
+            age=argv[4], \
+            last_name=argv[5])
+    print "New " + table_key + ":", record
+
+def run_delete():
     try:
         record = table.get(id=argv[3])
-        print "Delete:", record
         record.delete_instance()
         print "Delete:", record
     except:
         print "Nothing to delete"
 
 
-# if no arguments were passed (excluding the filename itself)
-if len(argv) == 1:
-    print "Please enter an action"
+# MAIN CODE
+# if no arguments were passed (excluding filename): print error message
+while True:
+    commands = {
+        'create': run_create,
+        'print': run_print,
+        'insert': run_insert,
+        'delete': run_delete
+    }
 
-elif argv[1] == "create":
-    # create all tables related to models
-    my_models_db.connect()
-    my_models_db.create_tables([BaseModel, School, Batch, User, Student])
-    # not sure of the merits of try/except clauses here
+    if len(argv) < 2:
+        # no action submitted: print error message
+        print "Please enter an action"
+    
+    elif argv[1] in commands.keys():
+        # action submitted matches predefined options;
+        # check if conditions to execute commands are met
+        command_key = argv[1]
+        models = { 'basemodel': BaseModel,
+               'school': School,
+               'batch': Batch,
+               'user': User,
+               'student': Student }
+        
+        if len(argv) < 3 and command == 'create': commands[command_key]()
+        elif len(argv) < 3: pass # wrong number of arguments for cmd
+        elif argv[2] not in models.keys(): # improper value passed for table
+            print "Undefined value", argv[2]
+        else:
+            table_key = argv[2]
+            table = models[table_key]
+            commands[command_key]()
 
-elif argv[1] == "print":
-    if len(argv) < 3: pass
-    # else print data for each record in requested table
-    else: print_table()
-
-elif argv[1] == "insert":
-    # execute insert command
-    if argv[2] == "school": record = School.create(name=argv[3])
-    elif argv[2] == "batch": record = Batch.create(school=argv[3], name=argv[4])
-    elif argv[2] == "student" and len(argv) == 7:
-        record = Student.create(
-            batch=argv[3], \
-            age=argv[4], \
-            last_name=argv[5], \
-            first_name=argv[6])
-    elif argv[2] == "student" and len(argv) == 6:
-        record = Student.create(
-            batch=argv[3], \
-            age=argv[4], \
-            last_name=argv[5])
-    print "New " + argv[2] + ":", record
-
-elif argv[1] == "delete":
-    if argv[2] == "school": delete_record(School)
-    elif argv[2] == "batch": delete_record(Batch)
-    elif argv[2] == "student": delete_record(Student)
-
-else:
-    print "Undefined action " + argv[1]    
-
+    else:
+        # action submitted does not match options: print error message
+        print "Undefined action", command
+        
+    break
