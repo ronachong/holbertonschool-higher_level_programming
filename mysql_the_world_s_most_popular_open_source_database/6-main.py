24fdb641d560d777f903d2dfc498c5fc1c3faf16
Rona Chong <ronachong@gmail.com>
2016-06-23 14:52:40 -0700

    Assign response value outside of if conditional; fix main code.
    Change server.socket.close() to httpd.socket.close as 'httpd' is
    variable name of http server, not 'server'.
    
    Assign response a value to see if any response gets printed, re-
    gardless of params length.

--- mysql_the_world_s_most_popular_open_source_database/6-main.py
+++ mysql_the_world_s_most_popular_open_source_database/6-main.py
@@ -9,8 +9,7 @@ In other words, the server acts as a JSON API for the database Project 169.
 
 # modules
 from BaseHTTPServer import BaseHTTPRequestHandler as Handler, HTTPServer
-# import SimpleHTTPServer as http <-- this would use a filesystem instead of code?
-import mysql.connector as mysql
+from helper import *
 
 # create handler class for http server (which only handles gets requests)
 class GET_handler(Handler):
@@ -20,11 +19,12 @@ class GET_handler(Handler):
         return params
 
     def do_GET(self):
-        self.send_response(200)                     # provide response code for no errors?
-        self.send_header('Content-type', 'json')    # specify the type of response served as header? not sure
-        self.end_headers()
+        #self.send_response(200)                     # provide response code for no errors?
+        #self.send_header('Content-type', 'json')    # specify the type of response served as header? not sure
+        #self.end_headers()
 
         params = self.get_params()
+        response = params
         if len(params) == 1 and params[0] == 'tvshows':
             response = get_tvshows()
 
@@ -39,48 +39,8 @@ def main():
         httpd.serve_forever()
     except KeyboardInterrupt:
         print '^C: shutting down server'
-        server.socket.close()
+        httpd.socket.close()
 
 if __name__ == '__main__':
     # execute only if run as a script
     main()
-
-# helper methods defined here
-def get_kvpair(cursor, id, table, attribute):
-    query = ("SELECT " +  attribute + " FROM " + table + " WHERE id=" + str(id) + ";")
-    cursor.execute(query)
-    value = cursor.fetchall()[0][0]
-    return (attribute, value)
-
-def get_tvshows():
-    # make a master list to contain the json struct
-    mlist = []
-    table_attrs = { 'TVShow': ['id', 'name', 'poster'],
-                    'Network': ['id', 'name'],
-                    'TVShowActor': ['tvshow_id', 'actor_id'],
-                    'Actor': ['id', 'name'],
-                    'TVShowGenre': ['tvshow_id', 'genre_id'],
-                    'Genre': ['id', 'name'],
-                    'Season': ['id', 'number', 'tvshow_id'],
-                    'Episode': ['id', 'name', 'number', 'overview', 'season_id']   }
-    attr_list = table_attrs['TVShow']
-    cnx = mysql.connect(user='student',
-                        password='aLQQLXGQp2rJ4Wy5',
-                        host='173.246.108.142',
-                        database='Project_169')
-    cursor = cnx.cursor()
-    query = ("SELECT id from TVShow;")
-
-    cursor.execute(query)
-    for record in cursor.fetchall():
-        id = record[0]
-        # run through attributes and create dict for record, using list mapping
-        dict_list = []
-        for attribute in attr_list:
-            value = get_kvpair(cursor, id, urlarg, attribute)
-            dict_list.append(value)
-        rec_dict = dict(dict_list)
-        mlist.append(rec_dict)
-
-    cnx.close()
-    return mlist
