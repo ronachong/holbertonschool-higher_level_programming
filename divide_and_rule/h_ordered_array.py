071badec84ec9a9ebac7f36b80a18c4ca8e22f97
Rona Chong <ronachong@gmail.com>
2016-06-25 19:53:22 -0700

    Create OrderedArray class and finish OrderedArrayThread class.

--- divide_and_rule/h_ordered_array.py
+++ divide_and_rule/h_ordered_array.py
@@ -1,9 +1,43 @@
-class OrderedArray
+import threading
+
+class OrderedArray:
+    list = []
+    lock = threading.Lock()
+    
+    def __init__(self):
+        pass
+
+    def add(self, number):
+        if type(number) != int: raise Exception("number is not an integer")
+        thread = OrderedArrayThread(number)
+        thread.start()
+
+    def isSorting(self):
+        if threading.active_count() != 1: return True
+        #else
+        return False
+
+    def __str__(self):
+        return str(OrderedArray.list)
+
 
 class OrderedArrayThread(threading.Thread):
     def __init__(self, number):
-        if type(number) != integer: raise Exception("number is not an integer")
+        if type(number) != int: raise Exception("number is not an integer")
+        threading.Thread.__init__(self)
         self.number = number
 
     def run(self):
-        OrderedArray.list.insert(i, self.number) # i not yet defined
+        OrderedArray.lock.acquire()
+        i = 0
+        for x in OrderedArray.list:
+            if x > self.number:
+                OrderedArray.list.insert(i, self.number)
+                OrderedArray.lock.release()
+                return
+            i += 1
+
+        # ELSE: all values in list were smaller, or there were no values; append at end.
+        OrderedArray.list.append(self.number)
+        OrderedArray.lock.release()
+
