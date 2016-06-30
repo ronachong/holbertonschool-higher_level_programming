9fc6ca311f4f02206231c2c03db16519d0f111c3
Rona Chong <ronachong@gmail.com>
2016-06-29 21:08:44 -0700

    Write preliminary iteration of Store class.
    Use a semaphore to cap the number of threads/person instances ent-
    ering store.
    
    Create lock as class attribute to ensure that item_number is read
    and altered accurately if multiple threads are "buying" an item at
    once.

--- splitting_tasks_to_be_faster/h_ip.py
+++ splitting_tasks_to_be_faster/h_ip.py
@@ -2,6 +2,13 @@ import threading
 import requests
 import json
 
+'''
+The IPThread constructor creates a thread which sends a GET request to the
+ip2country API with the IP passed to it as a parameter; parses the response for
+the country name of the IP, sends this name back to the callback passed to it,
+and prints the IP searched and country name retrieved.
+'''
+
 class IPThread(threading.Thread):
     def __init__(self, ip, callback):
         threading.Thread.__init__(self)
